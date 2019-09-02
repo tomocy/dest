15cf203c987a4551e84c20942ecf51d70b6e34e2
tomocy <tomocy.dev@gmail.com>
2019-09-02 13:39:25 +0900

    add parseConfig
    to parse flags to config

--- cmf/desk/main.go
+++ cmf/desk/main.go
@@ -1,6 +1,17 @@
 package main
 
+import "flag"
+
 func main() {}
+
+func parseConfig() config {
+	dir := flag.String("dir", "./", "the name of dir for files to be created")
+	flag.Parse()
+
+	return config{
+		dir: *dir, name: flag.Arg(0),
+	}
+}
 
 type config struct {
 	dir, name string
