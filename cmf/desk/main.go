61d294bbc37f157bf3551192ff222502b370c595
tomocy <tomocy.dev@gmail.com>
2019-09-02 13:41:10 +0900

    modify main
    to create desk with the parsed dir and name

--- cmf/desk/main.go
+++ cmf/desk/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
 
-func main() {}
+	"github.com/tomocy/desk"
+)
+
+func main() {
+	cnf := parseConfig()
+	if err := desk.Create(cnf.dir, cnf.name); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create desk: %s\n", err)
+		os.Exit(1)
+	}
+}
 
 func parseConfig() config {
 	dir := flag.String("dir", "./", "the name of dir for files to be created")
