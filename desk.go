10c21437bb92abfb2415df3efbfaba6eb1524369
tomocy <tomocy.dev@gmail.com>
2019-09-02 13:35:33 +0900

    add Create
    to create the pathname joined with the given dir and name

---
+++ desk.go
@@ -0,0 +1,86 @@
+package desk
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	solutionFilename = "main.go"
+	testFilename     = "main_test.go"
+)
+
+func Create(dir, name string) error {
+	adjusted := adjustName(name)
+	dir = filepath.Join(dir, adjusted)
+	if err := os.MkdirAll(dir, 0700); err != nil {
+		return err
+	}
+
+	solFname, testFname := filepath.Join(dir, solutionFilename), filepath.Join(dir, testFilename)
+	if err := createDeskFiles(solFname, testFname); err != nil {
+		return err
+	}
+
+	if err := writeSolution(solFname); err != nil {
+		return err
+	}
+	if err := writeTest(testFname); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func adjustName(name string) string {
+	lower := strings.ToLower(name)
+	return strings.ReplaceAll(lower, " ", "_")
+}
+
+func createDeskFiles(names ...string) error {
+	for _, name := range names {
+		f, err := os.Create(name)
+		if err != nil {
+			return err
+		}
+		if err := f.Close(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func writeSolution(name string) error {
+	return write(name, `package main
+
+func main() {}`,
+	)
+}
+
+func writeTest(name string) error {
+	return write(name, "package main")
+}
+
+func write(name, content string) error {
+	dst, err := os.OpenFile(name, os.O_WRONLY, 0700)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(dst, content)
+
+	return dst.Close()
+}
+
+func remove(names ...string) error {
+	for _, name := range names {
+		if err := os.RemoveAll(name); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ desk_test.go
@@ -0,0 +1,68 @@
+package desk
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+var testDataDir = filepath.Join(os.Getenv("GOPATH"), "/src/github.com/tomocy/desk/testdata")
+
+func TestCreate(t *testing.T) {
+	type file struct {
+		name, content string
+	}
+	type expected struct {
+		solution, test file
+	}
+	tests := []struct {
+		dir, name string
+		expected  expected
+	}{
+		{testDataDir, "Apple is red", expected{
+			file{filepath.Join(testDataDir, "apple_is_red", "main.go"), "package main\n\nfunc main() {}"},
+			file{filepath.Join(testDataDir, "apple_is_red", "main_test.go"), "package main"},
+		}},
+		{testDataDir, "Banana is yellow", expected{
+			file{filepath.Join(testDataDir, "banana_is_yellow", "main.go"), "package main\n\nfunc main() {}"},
+			file{filepath.Join(testDataDir, "banana_is_yellow", "main_test.go"), "package main"},
+		}},
+	}
+
+	assert := func(expected file) error {
+		f, err := os.Open(expected.name)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		actual, err := ioutil.ReadAll(f)
+		if err != nil {
+			return err
+		}
+
+		if string(actual) != expected.content {
+			return fmt.Errorf("unexpected content of file: got %q, expect %q", string(actual), expected.content)
+		}
+
+		return nil
+	}
+
+	for _, test := range tests {
+		if err := Create(test.dir, test.name); err != nil {
+			t.Errorf("unexpected error: got %s, expect nil\n", err)
+		}
+		if err := assert(test.expected.solution); err != nil {
+			t.Errorf("unexpected solution: %s\n", err)
+		}
+		if err := assert(test.expected.test); err != nil {
+			t.Errorf("unexpected test: %s\n", err)
+		}
+
+		remove(
+			filepath.Dir(test.expected.solution.name), filepath.Dir(test.expected.test.name),
+		)
+	}
+}
